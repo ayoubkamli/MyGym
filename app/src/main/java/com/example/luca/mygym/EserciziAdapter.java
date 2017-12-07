04fe03dfb990f21026f65ebfba0b718595f2d573
LukeTSC <luca.tarchi93@gmail.com>
2017-12-07 19:35:39 +0100

    implementata la lista degli esercizi

---
+++ app/src/main/java/com/example/luca/mygym/EserciziAdapter.java
@@ -0,0 +1,60 @@
+package com.example.luca.mygym;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+
+import java.util.ArrayList;
+
+public class EserciziAdapter extends BaseAdapter implements ListAdapter {
+    private ArrayList<String> list = new ArrayList<>();
+    private Context context;
+
+    EserciziAdapter(ArrayList<String> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return list.get(pos);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (inflater != null) {
+                view = inflater.inflate(R.layout.listview_row, null);
+            }
+        }
+
+        //Handle buttons and add onClickListeners
+        Button btn = view.findViewById(R.id.button_row);
+        btn.setText(list.get(position));
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        return view;
+    }
+}
