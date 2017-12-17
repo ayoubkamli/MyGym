dbed6d6514dc570fcf27367a99ed79d97b8b961a
LukeTSC <luca.tarchi93@gmail.com>
2017-12-17 23:48:52 +0100

    Implementato login con google, passa al main senza registrarsi sul db, bisogna mettere la chiamata a volley su una classe esterna

--- app/src/main/java/com/example/luca/mygym/LoginActivity.java
+++ app/src/main/java/com/example/luca/mygym/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.luca.mygym;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,14 +15,13 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -32,6 +33,7 @@ import com.google.android.gms.tasks.Task;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -49,118 +51,21 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public static final String PREFS_USR = "PrefsUser";
 
     //Variabili globali
-    public String Id_Facebook = "N.D.";
-    public String Id_Google = "N.D.";
-    public String Name = "N.D.";
-    public String Surname = "N.D.";
-    public String Birthday = "N.D.";
-    public String Gender = "N.D.";
-    public String Picture = "N.D.";
+    public HashMap<String, String> Profile = new HashMap<>();
+    public Bitmap Picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        callbackManager = CallbackManager.Factory.create();
-
-        LoginButton loginButton = findViewById(R.id.login_button);
-
+        //OnClickListener dei bottoni di facebook e google
+        findViewById(R.id.login_button).setOnClickListener(this);
         findViewById(R.id.sign_in_button).setOnClickListener(this);
 
-        final RequestQueue queue = Volley.newRequestQueue(this);
+        callbackManager = CallbackManager.Factory.create();
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-
-            @Override
-            public void onSuccess(LoginResult loginResult) {// App code
-
-                GraphRequest request = GraphRequest.newMeRequest(
-                        AccessToken.getCurrentAccessToken(),
-                        new GraphRequest.GraphJSONObjectCallback() {
-                            @Override
-                            public void onCompleted(
-                                    JSONObject object,
-                                    GraphResponse response) {
-                                try {
-                                    final String IdFacebook = object.getString("id");
-                                    final String Name = object.getString("first_name");
-                                    final String Surname = object.getString("last_name");
-                                    final String Birthday ;
-                                    if (object.has("birthday")) {
-                                        Birthday = object.getString("birthday");
-                                    } else {
-                                        Birthday = "null";
-                                    }
-                                    final String Gender = object.getString("gender");
-                                    final String Image_url = "http://graph.facebook.com/" + IdFacebook + "/picture?type=large";
-
-                                    // Instantiate the RequestQueue.
-                                    String uri = "http://tscmygym.altervista.org/reg.php";
-
-                                    // Request a string response from the provided URL.
-                                    StringRequest stringRequest = new StringRequest(Request.Method.POST, uri,
-                                            new Response.Listener<String>() {
-                                                @Override
-                                                public void onResponse(String response) {
-                                                    // Display the first 500 characters of the response string.
-                                                    Log.i("cazzosi", response);
-
-                                                }
-                                            }, new Response.ErrorListener() {
-                                        @Override
-                                        public void onErrorResponse(VolleyError error) {
-                                            Log.i("cazzono", "cazzo");
-                                        }
-                                    })
-                                    {
-                                        @Override
-                                        protected Map<String,String> getParams(){
-                                            Map<String,String> params = new HashMap<>();
-                                            params.put("Id", IdFacebook);
-                                            params.put("Name", Name);
-                                            params.put("Surname", Surname);
-                                            params.put("Birthday", Birthday);
-                                            params.put("Gender", Gender);
-                                            params.put("Image_url", Image_url);
-
-                                            return params;
-                                        }
-                                    };
-                                    // Add the request to the RequestQueue.
-                                    queue.add(stringRequest);
-
-                                    SharedPreferences.Editor editor = getSharedPreferences(PREFS_USR, MODE_PRIVATE).edit();
-                                    editor.putString("Id_Facebook", IdFacebook);
-                                    editor.apply();
-
-                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                    startActivity(intent);
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        });
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "id, first_name, last_name, birthday, gender");
-                request.setParameters(parameters);
-                request.executeAsync();
-            }
-
-            @Override
-            public void onCancel() {
-                // App code
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
-                // App code
-            }
-        });
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .build();
-
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
@@ -178,28 +83,63 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    // [START handleSignInResult]
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            Id_Google = account.getId();
-            Name = account.getGivenName();
-            Surname = account.getFamilyName();
-            Picture = String.valueOf(account.getPhotoUrl());
+            Profile.put("Id_Facebook", "N.D.");
+            Profile.put("Id_Google", account.getId());
+            Profile.put("Name", account.getGivenName());
+            Profile.put("Surname", account.getFamilyName());
+            Profile.put("Birthday", "N.D.");
+            Profile.put("Gender", "N.D.");
+            //Picture = String.valueOf(account.getPhotoUrl());
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
         }
     }
-    // [END handleSignInResult]
 
-    // [START signIn]
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
-    // [END signIn]
+
+    private void facebookLogin(final LoginResult loginResult) {
+        GraphRequest request = GraphRequest.newMeRequest(
+                loginResult.getAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        // Application code
+                        try {
+                            Log.i("Response",response.toString());
+
+                            Profile.put("Id_Facebook", response.getJSONObject().getString("id"));
+                            Profile.put("Id_Google", "N.D.");
+                            Profile.put("Name", response.getJSONObject().getString("first_name"));
+                            Profile.put("Surname", response.getJSONObject().getString("last_name"));
+                            if (response.getJSONObject().has("birthday")) {
+                                Profile.put("Birthday", response.getJSONObject().getString("birthday"));
+                            }
+                            Profile.put("Gender", response.getJSONObject().getString("gender"));
+
+                            /*try {
+                                URL imageUrl = new URL("https://graph.facebook.com/" + Id_Facebook + "/picture?type=large");
+                                Picture = BitmapFactory.decodeStream(imageUrl.openConnection().getInputStream());
+                            } catch (java.io.IOException e) {
+                                e.printStackTrace();
+                            }*/
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,email,first_name,last_name,gender");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
 
     @Override
     public void onClick(View v) {
@@ -207,9 +147,62 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.sign_in_button:
                 signIn();
                 break;
-            case R.id.login_fb:
-                facebookLogin();
+            case R.id.login_button:
+                LoginManager.getInstance().logInWithReadPermissions(
+                        this,
+                        Arrays.asList("email", "public_profile"));
+
+                LoginManager.getInstance().registerCallback(callbackManager,
+                        new FacebookCallback<LoginResult>() {
+                            @Override
+                            public void onSuccess(LoginResult loginResult) {
+                                facebookLogin(loginResult);
+                            }
+
+                            @Override
+                            public void onCancel() {
+                            }
+
+                            @Override
+                            public void onError(FacebookException exception) {
+                            }
+                        });
                 break;
         }
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        // Instantiate the RequestQueue.
+        String uri = "http://tscmygym.altervista.org/reg.php";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, uri,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.i("cazzoresp", response);
+                        Log.i("cazzoresp", String.valueOf(Profile));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("cazzono", "cazzo");
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                return Profile;
+            }
+        };
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
+        SharedPreferences.Editor editor = getSharedPreferences(PREFS_USR, MODE_PRIVATE).edit();
+        editor.putString("Id_Facebook", Profile.get("Id_Facebook"));
+        editor.putString("Id_Google", Profile.get("Id_Google"));
+        editor.apply();
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
