c3873caa10ae72ce2a82a956de8da2edbbaa044b
LukeTSC <luca.tarchi93@gmail.com>
2017-12-20 18:11:12 +0100

    SplashActivity finita e pulita

--- app/src/main/java/com/example/luca/mygym/SplashActivity.java
+++ app/src/main/java/com/example/luca/mygym/SplashActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.facebook.AccessToken;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 
 /**
  * La SplashActiviy è la prima activity a partire, serve a controllare se si è già loggati con
@@ -20,14 +21,17 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         //Variabile booleana che restituisce true se si è loggati con Facebook
-        final boolean loggedInFb;
-        loggedInFb = isFacebookLoggedIn();
+        final boolean loggedInFacebook;
+        loggedInFacebook = isFacebookLoggedIn();
+        //Variabile booleana che restituisce true se si è loggati con Google
+        final boolean loggedInGoogle;
+        loggedInGoogle = isSignedIn();
         //Funzione per mettere in pausa l'esecuzione e dare la possibilità di visualizzare l'immagine di background
         Handler handler = new Handler();
         handler.postDelayed(new Runnable(){
             @Override
             public void run(){
-                if (loggedInFb) {
+                if (loggedInFacebook || loggedInGoogle) {
                     Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                     startActivity(intent);
                 } else{
@@ -40,5 +44,8 @@ public class SplashActivity extends AppCompatActivity {
 
     public boolean isFacebookLoggedIn(){
         return AccessToken.getCurrentAccessToken() != null;
+    }
+    private boolean isSignedIn() {
+        return GoogleSignIn.getLastSignedInAccount(this) != null;
     }
 }
